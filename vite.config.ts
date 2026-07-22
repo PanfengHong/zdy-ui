c8e4659590df706e92b8c148b59e80454bd885b6
Patrick Hong <black_royal@163.com>
2026-07-22 12:34:29 +0800

    feat: no.2 vite迁移

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,31 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, './src'),
+    },
+  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(dirname, 'src/index.ts'),
       name: 'ReactUiComponentLibrary',
       fileName: format => `react-ui-component-library.${format}.js`
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['react', 'react-dom'],
       output: {
-        // 在UMD构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
         }
       }
     }
-  },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
   }
 });
